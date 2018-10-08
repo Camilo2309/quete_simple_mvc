fbbbd7a33385568b28fbb782ae9ebf56168b03c7
Camilo <camilo.bolanos23@hotmail.com>
2018-10-08 15:57:36 +0200

    MVC 3 EASYYYYY

--- public/index.php
+++ public/index.php
@@ -5,3 +5,19 @@ require __DIR__ . '/../vendor/autoload.php';
 $controller = new \Controller\ItemController();
 
 $controller->index();
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$route = $_GET['route'] ?? '';
+
+if ($route === 'items') {
+    $controller = new Controller\ItemController;
+    $controller->index();
+} elseif ($route === 'item') {
+    $controller = new Controller\ItemController;
+    // la méthode show() de ItemController est appelée et l'id indiqué dans la route lui est passé en paramètre
+    $controller->show($_GET['id']);
+} else {
+    header("HTTP/1.0 404 Not Found");
+    echo 'Page introuvable';
+}
